726397283b5bcdbbb28b8a36fec9fba16e2a2849
asrasmussen <asrasmussen@student.unimelb.edu.au>
2024-06-08 16:27:09 +1000

    made drop down work

--- javascript.js
+++ javascript.js
@@ -12,9 +12,69 @@ plus.addEventListener("click", ()=> {
 })
 
 minus.addEventListener("click", ()=> {
-    if(a>0){
+    if(a>1){
     a--;
     number.innerText = a;
     }
    
 })
+
+// let acc = document.getElementsByClassName("accordion");
+// let i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     /* Toggle between adding and removing the "active" class*/
+//     this.classList.toggle("active");
+
+//     /* Toggle between hiding and showing the active panel */
+//     let panel = this.nextElementSibling;
+//     panel = document.getElementById(acc[i].getAttribute("id") + "-text");
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+
+//Logic for accordions
+let shipping = document.getElementById("shipping");
+let faq = document.getElementById("faq");
+let details = document.getElementById("details");
+shipping.addEventListener("click", () => {
+    shipping.classList.toggle("active");
+    let panel = document.getElementById("shipping-text");
+    panelLogic(panel);
+});
+faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+    let panel = document.getElementById("faq-text");
+    panelLogic(panel);
+});
+details.addEventListener("click", () => {
+    details.classList.toggle("active");
+    let panel = document.getElementById("faq-text");
+    panelLogic(panel);
+});
+
+function panelLogic(panel) {
+    if (panel.style.display === "block") {
+        collapsePanel(panel);
+    } else {
+        collapseAllPanels();
+        expandPanel(panel);
+    }
+}
+
+function collapsePanel(panel) {
+    panel.style.display = "none";
+}
+function collapseAllPanels() {
+    collapsePanel(document.getElementById("details-text"));
+    collapsePanel(document.getElementById("shipping-text"));
+    collapsePanel(document.getElementById("faq-text"));
+}
+function expandPanel(panel) {
+    panel.style.display = "block";
+}
