d02aba4d0a3e8e238ce8c2698758e8a02b25e6a3
asrasmussen <asrasmussen@student.unimelb.edu.au>
2024-06-11 11:31:11 +1000

    made second halo page and updated cart, checkout pages

--- store.js
+++ store.js
@@ -16,6 +16,26 @@ function ready() {
 
     // Coupon code form validation
     document.getElementById('coupon-form').addEventListener('submit', applyCoupon);
+
+    // This script is for the incremental-wrapper //
+    const plus = document.querySelector(".plus"),
+    minus = document.querySelector(".minus"),
+    number = document.querySelector(".number");
+
+    let a = 1;
+
+    plus.addEventListener("click", ()=> {
+        a++;
+        number.innerText = a;
+    })
+
+    minus.addEventListener("click", ()=> {
+        if(a>1){
+        a--;
+        number.innerText = a;
+        }
+   
+})
 }
 
 function removeCartItem(event) {
